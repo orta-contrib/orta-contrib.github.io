c51bf8eaa3875365be1a00b03ca97fbd339431d3
orta-contrib <orta.contrib@gmail.com>
2020-09-26 23:56:49 +0900

    first commit

---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,12 @@
+import React from "react"
+/**
+ * Change the content to add your own bio
+ */
+
+export default function Bio() {
+  return (
+    <>
+      OSS Contribution RTA Player.
+    </>
+  )
+}
